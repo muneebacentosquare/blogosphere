f83503f5933d3f4641676d89b6fcb614a83fe379
Muneeba <muneeba.centosquare@gmail.com>
2024-05-29 12:18:37 +0500

    Login api in react

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -1,23 +1,40 @@
+import {useState} from "react";
+import AuthUser from "../../Components/AuthUser";
+
 export default function Login() {
+    const {http, setToken} = AuthUser();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+    const submitForm = () => {
+        http.post('auth/login', { email: email, password: password })
+            .then((res) => {
+                setToken(res.data.user, res.data.accessToken); // Call setToken from AuthUser
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
     return (
         <div className="flex items-center justify-center min-h-screen bg-gray-100">
             <div className="px-8 py-6 bg-white rounded-lg shadow-md text-left w-full max-w-md">
                 <h3 className="text-2xl font-bold text-center">Login</h3>
-                <form action="/login" method="post" className="mt-4">
+                <form className="mt-4">
                     <div className="mb-6">
                         <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">Email
                             Address</label>
-                        <input type="email" id="email" name="email"
+                        <input type="email" id="email" onChange={e => setEmail(e.target.value)}
                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:ring focus:ring-blue-500 focus:ring-opacity-50"/>
                     </div>
                     <div className="mb-6">
                         <label htmlFor="password"
                                className="block text-gray-700 text-sm font-bold mb-2">Password</label>
-                        <input type="password" id="password" name="password"
+                        <input type="password" id="password" onChange={e => setPassword(e.target.value)}
                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:ring focus:ring-blue-500 focus:ring-opacity-50"/>
                     </div>
                     <div className="flex items-center justify-between">
-                        <button type="submit"
+                        <button type="button" onClick={submitForm}
                                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring focus:ring-blue-500 focus:ring-opacity-50">Login
                         </button>
                         <a href="#"
